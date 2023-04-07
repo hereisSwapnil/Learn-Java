c1da339e62a47d2ae6638325337190df2c8d5b8f
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-07 11:16:03 +0530

    added input lines

--- data_types.java
+++ data_types.java
@@ -2,16 +2,22 @@ public class data_types{
     public static void main(String args[]){
         // Byte Data type
         byte b = 8;
+        System.out.println(b);
         // Char Data type - stores character type in here
         char ch = 'a';
+        System.out.println(ch);
         // Boolean Data type - stores true/false values 
         boolean var = true;
+        System.out.println(var);
         // Float Data type - stores decimal type values
         float price = 10.5f;
+        System.out.println(price);
         // Integer Data type - stores integer type values (no decimals are included)
         int number = 10;
+        System.out.println(number);
         // long
         long long_int = 12345678910L;
+        System.out.println(long_int);
         // double
         // short
     }
