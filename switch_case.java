ef2b8dafe1f76237fa17d03ae6267d589f39983e
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-09 10:40:13 +0530

    switch statements

---
+++ switch_case.java
@@ -0,0 +1,14 @@
+public class switch_case{
+    public static void main(String args[]){
+        int n = 3;
+        switch(n){
+            case 1: System.out.println("This is one");
+                break;
+            case 2: System.out.println("This is two");
+                break;
+            case 3: System.out.println("This is three");
+                break;
+            default: System.out.println("This is default");
+        }
+    }
+}
