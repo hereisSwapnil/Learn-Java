98ddd50eebdebb48d188e261a822d4bcc1738c19
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-09 11:05:44 +0530

    fever check

---
+++ Basic Programs/fever_check.java
@@ -0,0 +1,6 @@
+public class fever_check{
+    public static void main(String args[]){
+        double temp = 103.5;
+        System.out.println( (temp>100)?"You have fever":"You don't have fever" );
+    }
+}
