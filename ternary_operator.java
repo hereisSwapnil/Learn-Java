69cf397ccc7c00af879178276a61d85fb9d4cafc
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-09 10:40:04 +0530

    ternary operator

---
+++ ternary_operator.java
@@ -0,0 +1,7 @@
+public class ternary_operator{
+    public static void main(String args[]){
+        int n = 9;
+        String type = (n%2==0)? "Even" : "Odd";
+        System.out.println(type);
+    }
+}
