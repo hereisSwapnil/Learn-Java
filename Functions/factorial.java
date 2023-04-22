24c7b11338c48f566ce1c01784ad41026a1a88d0
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-22 11:43:23 +0530

    added functions

---
+++ Functions/factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+    public static int factorial (int a){
+        int fac = 1;
+        for (int i = a; i>=1; i--){
+            fac*=i;
+        }
+        return fac;
+    }
+    public static void main(String args[]){
+        int a = 5;
+        System.out.print("Factorial of a is ");
+        System.out.println(factorial(a));
+    }
+}
---
+++ Functions/functions.java
@@ -0,0 +1,10 @@
+public class functions{
+    public static void printSum(int a , int b){
+        System.out.println(a+b);
+    }
+    public static void main(String args[]){
+        int a = 4;
+        int b = 5;
+        printSum(a,b);  
+    }
+}
