2bb178ee38a7dd40128c609362a71fd690b7c534
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-23 04:49:51 +0530

    patterns added

---
+++ Patterns/hollow_rectangle.java
@@ -0,0 +1,20 @@
+public class hollow_rectangle{
+    public static void main(String args[]){
+        int row = 4; //     ---
+        int column = 5; //   |
+        for(int i = 1; i<=row ;i++){
+            for(int j = 1; j<=column; j++){
+                if(i == 1 || i == row){
+                    System.out.print("*");
+                }
+                else if(j == 1 || j == column){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
