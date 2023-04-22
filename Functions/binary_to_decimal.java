c5f7f7834cfc47accdea717360fded8d8e64bf8c
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-23 04:17:46 +0530

    binary to decimal number conversion code

--- Functions/binary_to_decimal.java
+++ Functions/binary_to_decimal.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class binary_to_decimal{
     public static int bin_dec(int a){
-        int c = 0;
-        int dec = 0;
+        int pow = 0;
         int temp;
+        int DecNum = 0;
         while(a>0){
-            temp = (int) ((a%10)*(Math.pow(2,c)));
-            dec += temp ;
+            temp = (int) ((a%10)*(Math.pow(2,pow)));
+            DecNum += temp ;
             a = a/10;
-            c+=1;
+            pow+=1;
         }
-        return dec;
+        return DecNum;
     }
     public static void main(String args[]){
         int a = 1010010100;
