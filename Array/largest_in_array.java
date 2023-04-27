8ce3a2f9be8e338455e05ce26f4dad39192f412e
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-27 12:37:48 +0530

    largest number in array

---
+++ Array/largest_in_array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class largest_in_array{
+    public static int LargestInArray(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i<numbers.length; i++){
+            if(numbers[i]>largest){
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String args[]){
+        int numbers[] = {2,5,7,4,9,75,53,241,45};
+        System.out.println(LargestInArray(numbers));
+    }
+}
