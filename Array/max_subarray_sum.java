d4a516129923e309c470f702f6e91012ebbd502d
Swapnil Kumar <swapnilskumars@gmail.com>
2023-04-27 12:38:35 +0530

    get max subarray sum

---
+++ Array/max_subarray_sum.java
@@ -0,0 +1,22 @@
+public class max_subarray_sum{
+    public static int max_subarray_sum(int numbers[]){
+        int Sum = 0;
+        for(int i = 0; i<numbers.length; i++){
+            for(int j = i; j<numbers.length; j++){
+                int temp_sum = 0;
+                for(int k = i; k<=j; k++){
+                    temp_sum += numbers[k];
+                }
+                if(temp_sum>Sum){
+                    Sum = temp_sum;
+                }
+            }
+            return Sum;
+        }
+        return 0;
+    }
+    public static void main(String args[]){
+        int numbers[] = {1,-2,6,-1,3};
+        System.out.println("Max Subarray Sum = "+max_subarray_sum(numbers));
+    }
+}
