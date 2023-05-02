594df62ddb0b048737b8d759d10e065875019a9c
Swapnil Kumar <swapnilskumars@gmail.com>
2023-05-02 17:47:22 +0530

    added kadanes algorithm with all negative numbers

---
+++ Array/kadanes_algorithm_for_all_negative_values.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class kadanes_algorithm_for_all_negative_values{
+    public static int kadanes_max_sum_subarray(int numbers[]){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<numbers.length; i++){
+            if( numbers[i] > max){
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String args[]){
+        int numbers[] = {-1,-2,-6,-1,-3};
+        System.out.println("Max Subarray Sum = "+kadanes_max_sum_subarray(numbers));
+    }
+}
