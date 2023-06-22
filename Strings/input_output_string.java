a989c0da0210731661963536f80c0c9ea67fb701
Swapnil Singh <swapnilskumars@gmail.com>
2023-06-22 23:13:02 +0530

    added Strings

---
+++ Strings/input_output_string.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class input_output_string {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Our name is : "+name);
+        sc.close();
+    }
+}
---
+++ Strings/print_first_letter.java
@@ -0,0 +1,6 @@
+public class print_first_letter {
+    public static void main(String args[]){
+        String name = "Swamp";
+        System.out.println("First letter of name is : "+name.charAt(0));
+    }    
+}
