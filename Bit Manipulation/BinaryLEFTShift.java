33cad3100349634536e60b869f2895b879cb3375
Swapnil Singh <swapnilskumars@gmail.com>
2023-06-27 01:13:19 +0530

    added Binary LEFT/RIGHT Shift

---
+++ Bit Manipulation/BinaryLEFTShift.java
@@ -0,0 +1,7 @@
+public class BinaryLEFTShift {
+    public static void main(String args[]){
+        // LEFT SHIFT Operator - <<
+        // 5 << 2 = means shift binary 5 to the left by two places
+        System.out.println(5<<2);
+    }    
+}
---
+++ Bit Manipulation/BinaryRIGHTShift.java
@@ -0,0 +1,7 @@
+public class BinaryRIGHTShift {
+    public static void main(String args[]){
+        // RIGHT SHIFT Operator - >>
+        // 6 << 1 = means shift binary 6 to the right by one place
+        System.out.println(6>>1);
+    }     
+}
