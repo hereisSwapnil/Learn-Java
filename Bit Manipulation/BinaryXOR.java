b0b3d58c2821ce3b58536bc14c3afec0c6f7e37e
Swapnil Singh <swapnilskumars@gmail.com>
2023-06-27 01:13:35 +0530

    Binary XOR

---
+++ Bit Manipulation/BinaryXOR.java
@@ -0,0 +1,13 @@
+public class BinaryXOR {
+    public static void main(String args[]){
+        // Binary XOR - ^
+        // 0 ^ 0 - 0
+        // 0 ^ 1 - 1
+        // 1 ^ 0 - 1
+        // 1 ^ 1 - 0
+        System.out.println(0 ^ 0);
+        System.out.println(0 ^ 1);
+        System.out.println(1 ^ 0);
+        System.out.println(1 ^ 1);
+    }
+}
