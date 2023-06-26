588676582e551deb4a3d8eafc6519d377bef2b89
Swapnil Singh <swapnilskumars@gmail.com>
2023-06-27 01:15:34 +0530

    Clear range of i bits

---
+++ Bit Manipulation/Clear_range_of_Bits.java
@@ -0,0 +1,14 @@
+// public class Clear_range_of_Bits {
+//     public static int Clear_range_of__Bits(int num, int start, int end){
+//         int Left_BitMask = Math.pow(2,end+1) - 1;
+//         int Right_BitMask = 
+//     }
+//     public static void main(String args[]){
+//         int n = 150;
+//         int start = 1;
+//         int end = 4;
+//         // 010010110
+//         //     |  | 
+//         System.out.println(Clear_range_of__Bits(n, start, end));
+//     }
+// }
