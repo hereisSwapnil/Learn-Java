0a1abca285b404fed99a44f66efbfc098a3633ca
Swapnil Singh <swapnilskumars@gmail.com>
2023-06-27 01:13:01 +0530

    added binary AND OR

---
+++ Bit Manipulation/BinaryAND.java
@@ -0,0 +1,13 @@
+public class BinaryAND {
+    public static void main(String args[]){
+        // Binary AND - &
+        // 0 & 0 - 0
+        // 0 & 1 - 0
+        // 1 & 0 - 0
+        // 1 & 1 - 1
+        System.out.println(0 & 0);
+        System.out.println(0 & 1);
+        System.out.println(1 & 0);
+        System.out.println(1 & 1);
+    }
+}
---
+++ Bit Manipulation/BinaryOR.java
@@ -0,0 +1,13 @@
+public class BinaryOR {
+    public static void main(String args[]){
+        // Binary OR - |
+        // 0 | 0 - 0
+        // 0 | 1 - 0
+        // 1 | 0 - 0
+        // 1 | 1 - 0
+        System.out.println(0 | 0);
+        System.out.println(0 | 1);
+        System.out.println(1 | 0);
+        System.out.println(1 | 1);
+    }    
+}
