b2be0798f5f883ee5c650e4cc3e0aa5cc8de9ccb
Swapnil Singh <swapnilskumars@gmail.com>
2024-01-23 03:41:04 +0530

    updated till binary trees

---
+++ Binary Trees/Top_View_Of_A_Binary_Tree.java
@@ -0,0 +1,27 @@
+public class Top_View_Of_A_Binary_Tree {
+    static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void topView(Node root) {
+        // Level Traversal Order
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(7);
+        root.right.left = new Node(6);
+    }
+}
